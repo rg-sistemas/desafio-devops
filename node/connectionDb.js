8f26640c7daff87709101ebd43de6c66f1b1557a
victornico15 <vnespindola15@gmail.com>
2022-07-13 23:56:43 -0300

    Commit primeiro dia

--- node/connectionDb.js
+++ node/connectionDb.js
@@ -4,10 +4,10 @@ const config = {
     host: process.env.HOST || 'db',
     user: process.env.USER || 'root',
     password: process.env.PASSWORD || 'root',
-    database: process.env.DATABASE || 'nodedb'
+    database: process.env.DATABASE || "node_db",
 };
 
-const mysql = require('mysql');
+const mysql = require("mysql");
 const connection = mysql.createConnection(config);
 
 module.exports = connection;
